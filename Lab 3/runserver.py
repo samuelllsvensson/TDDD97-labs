a87a570b42fd674e0bf776ea47b84d23018b8079
samuelllsvensson <samuel.svensson.95@gmail.com>
2020-03-11 18:42:08 +0100

    Finished lab 3

--- Lab 3/runserver.py
+++
@@ -1,7 +0,0 @@
-from gevent.pywsgi import WSGIServer
-from geventwebsocket.handler import WebSocketHandler
-from Twidder import app
-
-app.debug = True
-http_server = WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
-http_server.serve_forever()
